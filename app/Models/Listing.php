9d5b5540b44eeba0192f19ab9acfe05b7edca05c
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-08-09 20:48:32 +0700

    feat: add create and store functionality

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,6 +9,7 @@ class Listing extends Model
 {
     use HasFactory;
 
+    protected $guarded = ['id'];
     public function scopeFilter($query, array $filters)
     {
         if ($filters['tag'] ?? false) {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ListingController;
-use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ListingController::class, 'index']);
+
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+Route::post('/listings', [ListingController::class, 'store']);
 
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
