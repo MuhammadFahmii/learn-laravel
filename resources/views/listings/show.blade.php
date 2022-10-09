70c1ee7aba477b3819daf161af86b69a2f770edc
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-10-09 13:03:28 +0700

    feat: add update and delete functionality

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -4,8 +4,7 @@
   <div class="mx-4">
     <x-card class="p-10">
       <div class="flex flex-col items-center justify-center text-center">
-        <img class="w-48 mr-6 mb-6"
-          src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png')}}" alt="" />
+        <img class="w-48 mr-6 mb-6" src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png')}}" alt="" />
 
         <h3 class="text-2xl mb-2">
           {{$listing->title}}
@@ -23,20 +22,16 @@
           <div class="text-lg space-y-6">
             {{$listing->description}}
 
-            <a href="mailto:{{$listing->email}}"
-              class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"><i
-                class="fa-solid fa-envelope"></i>
+            <a href="mailto:{{$listing->email}}" class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"><i class="fa-solid fa-envelope"></i>
               Contact Employer</a>
 
-            <a href="{{$listing->website}}" target="_blank"
-              class="block bg-black text-white py-2 rounded-xl hover:opacity-80"><i class="fa-solid fa-globe"></i>
+            <a href="{{$listing->website}}" target="_blank" class="block bg-black text-white py-2 rounded-xl hover:opacity-80"><i class="fa-solid fa-globe"></i>
               Visit Website</a>
           </div>
         </div>
       </div>
     </x-card>
-
-    {{-- <x-card class="mt-4 p-2 flex space-x-6">
+    <x-card class="mt-4 p-2 flex space-x-6">
       <a href="/listings/{{$listing->id}}/edit">
         <i class="fa-solid fa-pencil"></i> Edit
       </a>
@@ -46,6 +41,6 @@
         @method('DELETE')
         <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
       </form>
-    </x-card> --}}
+    </x-card>
   </div>
 </x-layout>
